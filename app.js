08b17b0c9280cbc3a1e4cc7f30ae6aeb43533845
Rohit Mahajan <rohitmahajan6942089@gamil.com>
2025-06-04 21:22:34 +0530

    created item container and carcusel

--- app.js
+++ app.js
@@ -1,3 +1,51 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const howItWorksBtn = document.getElementById("howItWorksBtn");
+    const howItworkSection = document.getElementById("howItwork");
+    const closeBtn = document.getElementById("closeHowItwork");
+
+    howItWorksBtn.addEventListener("click", () => {
+        howItworkSection.classList.remove("hidden");
+        howItworkSection.classList.add("flex");
+    });
+
+    closeBtn.addEventListener("click", () => {
+        howItworkSection.classList.add("hidden");
+        howItworkSection.classList.remove("flex");
+    });
+});
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const howItWorksBtn = document.getElementById("howItWorksBtn");
+    const howItworkSection = document.getElementById("howItwork");
+    const closeHowItworkBtn = document.getElementById("closeHowItwork");
+
+    const listYourBusinessBtn = document.getElementById("listYourBusinessBtn");
+    const listYourBusinessSection = document.getElementById("listYourBusiness");
+    const closeListBusinessBtn = document.getElementById("closeListBusiness");
+
+    howItWorksBtn.addEventListener("click", () => {
+        howItworkSection.classList.remove("hidden");
+        howItworkSection.classList.add("flex");
+    });
+
+    closeHowItworkBtn.addEventListener("click", () => {
+        howItworkSection.classList.add("hidden");
+        howItworkSection.classList.remove("flex");
+    });
+
+    listYourBusinessBtn.addEventListener("click", () => {
+        listYourBusinessSection.classList.remove("hidden");
+        listYourBusinessSection.classList.add("flex");
+    });
+
+    closeListBusinessBtn.addEventListener("click", () => {
+        listYourBusinessSection.classList.add("hidden");
+        listYourBusinessSection.classList.remove("flex");
+    });
+});
+
+
 function filterStores() {
     const input = document.getElementById("productSearch").value.toLowerCase();
     const cards = document.querySelectorAll(".store-card");
@@ -6,3 +54,4 @@ function filterStores() {
         card.style.display = product.includes(input) ? "flex" : "none";
     });
 }
+
